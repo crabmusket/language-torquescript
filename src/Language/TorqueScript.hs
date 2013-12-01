3d2aa1772c01eeff65ac0906bf5680c388da8aeb
Daniel Buckmaster <dan.buckmaster@gmail.com>
2013-12-02 08:32:50 +1100

    Why was that not there.

--- src/Language/TorqueScript.hs
+++ src/Language/TorqueScript.hs
@@ -1,5 +1,7 @@
 module Language.TorqueScript (
-    module Language.TorqueScript.AST
+    module Language.TorqueScript.AST,
+    module Language.TorqueScript.Parser
  ) where
 
 import Language.TorqueScript.AST
+import Language.TorqueScript.Parser
